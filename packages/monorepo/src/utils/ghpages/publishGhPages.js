8e9e1c55692ab87352b8184495d781f265b73d1d
Romain Lenzotti <romain.lenzotti@gmail.com>
2020-07-11 12:50:09 +0200

    fix: Add missing import to write CNAME for github pages

--- packages/monorepo/src/utils/ghpages/publishGhPages.js
+++ packages/monorepo/src/utils/ghpages/publishGhPages.js
@@ -1,20 +1,20 @@
 import { git } from '../cli/Git'
+import { writeFileSync } from 'fs'
 
 export async function publishGhPages (context) {
   const {
     ghpages,
-    productionBranch,
     version,
-    ghToken,
+    ghToken
   } = context
 
   if (ghpages.dir) {
-    fs.writeFileSync(`${ghpages.dir}/CNAME`, ghpages.cname, {})
+    writeFileSync(`${ghpages.dir}/CNAME`, ghpages.cname, {})
 
     git.init().cwd(ghpages.dir).sync()
     git.add('-A').cwd(ghpages.dir).sync()
     git.commit('-m', `'Deploy documentation v${version}'`).cwd(ghpages.dir).sync()
 
-    await git.push('--set-upstream', '-f', `https://${ghToken}@${ghpages.url}`, `${productionBranch}:${ghpages.branch}`).cwd(ghpages.dir)
+    await git.push('--set-upstream', '-f', `https://${ghToken}@${ghpages.url}`, `master:${ghpages.branch}`).cwd(ghpages.dir)
   }
 }
