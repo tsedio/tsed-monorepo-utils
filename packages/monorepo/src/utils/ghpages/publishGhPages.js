8beeabd406d250cebdb77841fadb7d4fc2006464
Romain Lenzotti <romain.lenzotti@gmail.com>
2020-08-08 09:38:50 +0200

    fix: Fix url repository to push page on ghPages

--- packages/monorepo/src/utils/ghpages/publishGhPages.js
+++ packages/monorepo/src/utils/ghpages/publishGhPages.js
@@ -9,12 +9,14 @@ export async function publishGhPages (context) {
   } = context
 
   if (ghpages.dir) {
+    const repository = ghpages.url.split('://')[1] || ghpages.url
+
     writeFileSync(`${ghpages.dir}/CNAME`, ghpages.cname, {})
 
     await git.init().cwd(ghpages.dir)
     await git.add('-A').cwd(ghpages.dir)
     await git.commit('-m', `'Deploy documentation v${version}'`).cwd(ghpages.dir)
 
-    await git.push('--set-upstream', '-f', `https://${ghToken}@${ghpages.url}`, `master:${ghpages.branch}`).cwd(ghpages.dir)
+    await git.push('--set-upstream', '-f', `https://${ghToken}@${repository}`, `master:${ghpages.branch}`).cwd(ghpages.dir)
   }
 }
