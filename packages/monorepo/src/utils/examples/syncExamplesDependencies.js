7fe1d875b5fd0ac1545d4a6a96670bbd1766e678
Romain Lenzotti <rom.lenzotti@gmail.com>
2020-09-28 19:23:04 +0200

    fix: Fix syncExamplesDependencies resolver versions

--- packages/monorepo/src/utils/examples/syncExamplesDependencies.js
+++ packages/monorepo/src/utils/examples/syncExamplesDependencies.js
@@ -1,17 +1,38 @@
 import chalk from 'chalk'
-import { join } from 'path'
 import { existsSync } from 'fs'
-import { updateVersions } from '../packages/updateVersions'
+import { join } from 'path'
 import { getDependencies } from '../depencencies/getDependencies'
+import { findPackages } from '../packages/findPackages'
 import { readPackage } from '../packages/readPackage'
+import { updateVersions } from '../packages/updateVersions'
 import { writePackage } from '../packages/writePackage'
 import { findExamples } from './findExamples'
+
+async function resolveDependencies (context) {
+  const { rootDir, packagesDir = 'packages' } = context
+  const dependenciesMap = await getDependencies(join(rootDir, 'package.json'))
+
+  const packages = await findPackages({ cwd: join(rootDir, packagesDir) })
+
+  packages.forEach(({ pkg: { name, version, devDependencies, dependencies } }) => {
+    dependenciesMap.set(name, version)
+
+    Object.entries({
+      ...dependencies,
+      ...devDependencies
+    }).forEach(([key, value]) => {
+      dependenciesMap.set(key, value.replace(/^\^/, ''))
+    })
+  })
+
+  return dependenciesMap
+}
 
 async function updatePackage (pkgPath, context) {
   const { logger, rootDir, silent = false } = context
 
   const currentPkg = await readPackage(pkgPath)
-  const dependencies = await getDependencies(join(rootDir, 'package.json'))
+  const dependencies = await resolveDependencies(context)
 
   !silent && logger('Update package', chalk.cyan(pkgPath))
 
