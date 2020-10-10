b6b4f79985d45afc47262b13a56849da32bea3d5
Romain Lenzotti <rom.lenzotti@gmail.com>
2020-10-10 11:49:16 +0200

    feat: Print list with verbose option when CI env variable is true

--- packages/monorepo/src/utils/common/createTasksRunner.js
+++ packages/monorepo/src/utils/common/createTasksRunner.js
@@ -3,6 +3,6 @@ import Listr from 'listr'
 export function createTasksRunner (tasks, context) {
   return new Listr(tasks, {
     concurrent: false,
-    renderer: !context.verbose ? 'default' : 'verbose'
+    renderer: !context.verbose && !context.env.CI ? 'default' : 'verbose'
   }).run(context)
 }
