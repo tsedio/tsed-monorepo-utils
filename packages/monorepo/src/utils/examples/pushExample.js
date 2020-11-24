90272d591551388e72ee9127c3ccbec471069f5c
Romain Lenzotti <rom.lenzotti@gmail.com>
2020-11-24 08:29:27 +0100

    fix: Add push tag action

--- packages/monorepo/src/utils/examples/pushExample.js
+++ packages/monorepo/src/utils/examples/pushExample.js
@@ -7,6 +7,12 @@ export function pushBranchExample (projectOptions, context) {
   return git.push('--set-upstream', '-f', remoteUrl, `${branch}:${branch}`).cwd(tmpDir)
 }
 
+export function pushTagsExample (projectOptions) {
+  const { tmpDir, remoteUrl } = projectOptions
+
+  return git.push(remoteUrl, `--tags`).cwd(tmpDir)
+}
+
 export function pushMainBranchExample (projectOptions) {
   const { tmpDir, mainBranch, remoteUrl } = projectOptions
   return git.push('--set-upstream', '-f', remoteUrl, `${mainBranch}:${mainBranch}`).cwd(tmpDir)
--- packages/monorepo/src/utils/examples/syncExample.js
+++ packages/monorepo/src/utils/examples/syncExample.js
@@ -1,80 +1,90 @@
-import chalk from "chalk";
-import { clean } from "../common/clean";
-import { createTasksRunner } from "../common/createTasksRunner";
-import { checkoutExample } from "./checkoutExample";
-import { commitAndMergeExample } from "./commitAndMergeExample";
-import { copyExampleFromSource, copyExampleFromWorkspace } from "./copyExample";
-import { createExampleOptions } from "./createExampleOptions";
-import { findExamples } from "./findExamples";
-import { installExampleDependencies } from "./installExampleDependencies";
-import { pushBranchExample, pushMainBranchExample } from "./pushExample";
-import { syncExampleDependencies } from "./syncExamplesDependencies";
+import chalk from 'chalk'
+import { clean } from '../common/clean'
+import { createTasksRunner } from '../common/createTasksRunner'
+import { checkoutExample } from './checkoutExample'
+import { commitAndMergeExample } from './commitAndMergeExample'
+import { copyExampleFromSource, copyExampleFromWorkspace } from './copyExample'
+import { createExampleOptions } from './createExampleOptions'
+import { findExamples } from './findExamples'
+import { installExampleDependencies } from './installExampleDependencies'
+import { pushBranchExample, pushMainBranchExample, pushTagsExample } from './pushExample'
+import { syncExampleDependencies } from './syncExamplesDependencies'
 
-export function syncExample(projectOptions, context) {
+export function syncExample (projectOptions, context) {
   return createTasksRunner([
     {
-      title: "Checkout",
+      title: 'Checkout',
       task: () => checkoutExample(projectOptions, context)
     },
     {
-      title: "Copy sources",
+      title: 'Copy sources',
       task: () => copyExampleFromSource(projectOptions, context)
     },
     {
-      title: "Sync dependencies",
+      title: 'Sync dependencies',
       task: () => syncExampleDependencies(projectOptions, context)
     },
     {
-      title: "Install dependencies",
+      title: 'Install dependencies',
       task: () => installExampleDependencies(projectOptions).toObservable()
     },
     {
-      title: "Copy workspace",
+      title: 'Copy workspace',
       enabled: () => projectOptions.sync,
       task: () => copyExampleFromWorkspace(projectOptions, context)
     },
     {
-      title: "Commit merge",
+      title: 'Commit merge',
       enabled: () => projectOptions.push,
       task: () => commitAndMergeExample(projectOptions, context)
     },
     {
-      title: "Push on main branch",
+      title: 'Push on main branch',
       enabled: () => projectOptions.push && projectOptions.pushOnMain,
       task: () => pushMainBranchExample(projectOptions, context).toObservable()
     },
     {
-      title: "Push on branch because there is conflict",
+      title: 'Push on branch because there is conflict',
       enabled: () => projectOptions.push && !projectOptions.pushOnMain,
       task: () => pushBranchExample(projectOptions, context).toObservable()
     },
     {
-      title: "Copy updated to file to source",
+      title: 'Push tags',
+      enabled: () => projectOptions.push,
+      task: async () => {
+        try {
+          await pushTagsExample(projectOptions, context)
+        } catch (er) {
+        }
+      }
+    },
+    {
+      title: 'Copy updated to file to source',
       enabled: () => projectOptions.sync,
       task: () => clean([projectOptions.tmpDir])
     }
-  ], { ...context, run: false, concurrent: false });
+  ], { ...context, run: false, concurrent: false })
 }
 
-export function syncExamples(context) {
+export function syncExamples (context) {
   const tasks = findExamples(context).map((project) => {
-    const { examples: { repositories = {} } } = context;
-    const currentExample = repositories[project];
+    const { examples: { repositories = {} } } = context
+    const currentExample = repositories[project]
 
     if (currentExample) {
-      const projectOptions = createExampleOptions(project, context);
-      projectOptions.sync = true;
+      const projectOptions = createExampleOptions(project, context)
+      projectOptions.sync = true
       return {
-        title: "Sync example '" + chalk.cyan(project) + "'",
+        title: 'Sync example \'' + chalk.cyan(project) + '\'',
         task: () => syncExample(projectOptions, context)
-      };
+      }
     }
   })
-    .filter(Boolean);
+    .filter(Boolean)
 
   return createTasksRunner(tasks, {
     ...context,
     run: false,
     concurrent: context.env.CI ? false : 4
-  });
+  })
 }
