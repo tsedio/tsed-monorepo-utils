c76a342677966bb51cef0b4b7f405062716169aa
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-01-30 22:16:43 +0100

    fix: Fix issue when pattern is too look

--- packages/monorepo/src/utils/packages/copyPackages.js
+++ packages/monorepo/src/utils/packages/copyPackages.js
@@ -1,16 +1,12 @@
 import { existsSync, readFileSync } from 'fs'
-import { findPackages } from './findPackages'
-import { copy } from '../common/copy'
 import { join } from 'path'
+import { copy } from '../common/copy'
+import { findPackages } from './findPackages'
 
 export async function copyPackages ({ rootDir, outputDir, packagesDir = 'packages', silent = false, ignore = [] }) {
   const packages = await findPackages({
     cwd: join(rootDir, packagesDir)
   })
-
-  let names = packages.map(({ name }) => name).join(',')
-
-  names = names.includes(',') ? `{${names}}` : names
 
   const ignores = packages.reduce((ignores, { path, name }) => {
     const file = path.replace('package.json', '.npmignore')
@@ -26,9 +22,17 @@ export async function copyPackages ({ rootDir, outputDir, packagesDir = 'package
     return ignores
   }, [])
 
+  let patterns = packages
+    .reduce((patterns, { name }) => {
+      return [
+        ...patterns,
+        `${packagesDir}/${name}/**`,
+        `${packagesDir}/${name}/**/.*`
+      ]
+    }, [])
+
   await copy([
-      `${packagesDir}/${names}/**`,
-      `${packagesDir}/${names}/**/.*`,
+      ...patterns,
       ...ignores,
       `!${packagesDir}/*/tsconfig.compile.json`,
       `!${packagesDir}/*/test/**`,
