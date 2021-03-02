0d2a1a7e1f4d10441fde213a911fd97d2b66c810
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-03-02 08:32:40 +0100

    feat: Allow multiple ghpages publishing

--- packages/monorepo/src/utils/ghpages/publishGhPages.js
+++ packages/monorepo/src/utils/ghpages/publishGhPages.js
@@ -9,19 +9,23 @@ export async function publishGhPages (context) {
     env
   } = context
 
-  if (ghpages.dir) {
-    const repository = ghpages.url.split('://')[1] || ghpages.url
+  const promises = [].concat(ghpages).map(async () => {
+    if (ghpages.dir) {
+      const repository = ghpages.url.split('://')[1] || ghpages.url
 
-    writeFileSync(`${ghpages.dir}/CNAME`, ghpages.cname, {})
+      writeFileSync(`${ghpages.dir}/CNAME`, ghpages.cname, {})
 
-    await git.init().cwd(ghpages.dir)
-    await git.add('-A').cwd(ghpages.dir)
-    await git.commit('-m', `Deploy documentation v${version}`).cwd(ghpages.dir)
+      await git.init().cwd(ghpages.dir)
+      await git.add('-A').cwd(ghpages.dir)
+      await git.commit('-m', `Deploy documentation v${version}`).cwd(ghpages.dir)
 
-    if (env.CI) {
-      await git.push('--set-upstream', '-f', `https://${ghToken}@${repository}`, `master:${ghpages.branch}`).cwd(ghpages.dir)
-    } else {
-      await git.push('--set-upstream', '-f', `https://${repository}`, `master:${ghpages.branch}`).cwd(ghpages.dir)
+      if (env.CI) {
+        await git.push('--set-upstream', '-f', `https://${ghToken}@${repository}`, `master:${ghpages.branch}`).cwd(ghpages.dir)
+      } else {
+        await git.push('--set-upstream', '-f', `https://${repository}`, `master:${ghpages.branch}`).cwd(ghpages.dir)
+      }
     }
-  }
+  })
+
+  return Promise.all(promises)
 }
