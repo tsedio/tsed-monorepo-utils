717d47169d6b37e64e4b4b1b5d6e7c2f0c40ecba
Romain Lenzotti <rom.lenzotti@gmail.com>
2022-08-06 14:52:37 +0200

    fix: fix write package.json

--- packages/monorepo/src/utils/packages/writePackage.js
+++ packages/monorepo/src/utils/packages/writePackage.js
@@ -3,5 +3,5 @@ import {dirname} from "path";
 
 export async function writePackage(pkgPath, pkg) {
   fs.ensureDirSync(dirname(pkgPath));
-  return fs.writeJson(pkgPath, pkg, {space: 2});
+  return fs.writeFile(pkgPath, JSON.stringify(pkg, null, 2), {encoding: "utf8"});
 }
