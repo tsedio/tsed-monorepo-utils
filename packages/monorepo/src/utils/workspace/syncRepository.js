f91265d0cf46649c2d72a3425b8364ad7239be46
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-10-21 11:12:28 +0200

    fix: add task to sync also alpha, beta, rc branch

--- packages/monorepo/src/utils/workspace/syncRepository.js
+++ packages/monorepo/src/utils/workspace/syncRepository.js
@@ -4,18 +4,25 @@ import {git} from "../cli/index.js";
  * @param context {MonoRepo}
  */
 export async function syncRepository(context) {
-  const {logger, productionBranch, developBranch, origin} = context;
+  const {logger, productionBranch, developBranch, branchName, origin} = context;
   try {
-    logger.info(`Push to ${productionBranch}`);
+    logger.info(`Push ${productionBranch}`);
     await git.push("--quiet", "--set-upstream", origin, productionBranch);
 
     if (productionBranch !== developBranch) {
       logger.info(`Sync ${developBranch} with ${productionBranch}`);
       await git.push("-f", origin, `${productionBranch}:refs/heads/${developBranch}`);
     }
-
-    logger.info("Release tag are applied on git");
   } catch (er) {
     logger.error(String(er), er.stack);
+  }
+
+  if (["alpha", "beta", "rc"].includes(branchName)) {
+    try {
+      logger.info(`Push ${branchName}`);
+      await git.push("--quiet", "--set-upstream", origin, branchName);
+    } catch (er) {
+      logger.error(`Fail to Push ${branchName}`);
+    }
   }
 }
